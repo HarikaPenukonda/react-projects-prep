a3e7c34226b5d38d0b7c43394b578bf75e10c2fc
HarikaPenukonda <penukondaharika@gmail.com>
2026-06-25 15:57:46 -0500

    createRoot

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Accordian from './components/accordian'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-    {/* Accordian component */}
-    <Accordian/>
-    </>
+   <></>
   )
 }
 
