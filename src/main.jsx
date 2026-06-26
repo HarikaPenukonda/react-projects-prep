96546cac079febfdd21e221aee47edf9c8ee4a9b
HarikaPenukonda <penukondaharika@gmail.com>
2026-06-26 11:44:22 -0500

    Code refractor

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,6 @@
-// import reactDOM from "react-dom/client"
+import {createRoot} from "react-dom/client"
 
-// reactDOM.createRoot(document.getElementById('root')).render(
-//   <h1>Hello, React!</h1>
-// )
-
-// imperative coding - How it should be done : Describe to every step on how to do something, i will do
-const h1 = document.createElement("h1")
-h1.textContent = "This is imperative coding"
-h1.className = "header"
-document.getElementById("root").appendChild(h1)
+const root  = createRoot(document.getElementById('root'))
+root.render(
+  <h1>Hello, React!</h1>
+)
