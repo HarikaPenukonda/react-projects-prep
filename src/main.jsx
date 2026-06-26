37b51ec77428d9b53aedee1e051cb82d006c7171
HarikaPenukonda <penukondaharika@gmail.com>
2026-06-26 11:40:27 -0500

    Challenge - Imperative coding

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import reactDOM from "react-dom/client"
+// import reactDOM from "react-dom/client"
 
-function MainContent(){
-  return(
-    <h1>React is great</h1>
-  )
-}
+// reactDOM.createRoot(document.getElementById('root')).render(
+//   <h1>Hello, React!</h1>
+// )
 
-reactDOM.createRoot(document.getElementById('root')).render(
-  <MainContent/>
-)
+// imperative coding - How it should be done : Describe to every step on how to do something, i will do
+const h1 = document.createElement("h1")
+h1.textContent = "This is imperative coding"
+h1.className = "header"
+document.getElementById("root").appendChild(h1)
