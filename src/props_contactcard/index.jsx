2672de4b6e25bcc58f2e1f2c4513754b0ed84fcc
HarikaPenukonda <penukondaharika@gmail.com>
2026-07-01 14:55:02 -0500

    code refactoring

--- src/props_contactcard/index.jsx
+++ src/props_contactcard/index.jsx
@@ -1,3 +1,4 @@
+import Contact from './components/Contact'
 import './index.css'
 /**
  * Challenge:
@@ -9,95 +10,8 @@ import './index.css'
 
 export default function ContactCard() {
     return (
-        <div className="contacts">
-            <article className="contact-card">
-                <img 
-                    src="src/assets/mr-whiskerson.png"
-                    alt="Photo of Mr. Whiskerson"
-                />
-                <h3>Mr. Whiskerson</h3>
-                <div className="info-group">
-                    <img 
-                        src="src/assets/phone-icon.png" 
-                        alt="phone icon" 
-                    />
-                    <p>(212) 555-1234</p>
-                </div>
-                <div className="info-group">
-                    <img 
-                        src="src/assets/mail-icon.png" 
-                        alt="mail icon"
-                    />
-                    <p>mr.whiskaz@catnap.meow</p>
-                </div>
-            </article>
-            
-            <article className="contact-card">
-                <img 
-                    src="src/assets/fluffykins.png"
-                    alt="Photo of Fluffykins"
-                />
-                <h3>Fluffykins</h3>
-                <div className="info-group">
-                    <img 
-                        src="src/assets/phone-icon.png" 
-                        alt="phone icon" 
-                    />
-                    <p>(212) 555-2345</p>
-                </div>
-                <div className="info-group">
-                    <img 
-                        src="src/assets/mail-icon.png" 
-                        alt="mail icon"
-                    />
-                    <p>fluff@me.com</p>
-                </div>
-            </article>
-            
-            <article className="contact-card">
-                <img 
-                    src="src/assets/felix.png"
-                    alt="Photo of Felix"
-                />
-                <h3>Felix</h3>
-                <div className="info-group">
-                    <img 
-                        src="src/assets/phone-icon.png" 
-                        alt="phone icon" 
-                    />
-                    <p>(212) 555-4567</p>
-                </div>
-                <div className="info-group">
-                    <img 
-                        src="src/assets/mail-icon.png" 
-                        alt="mail icon"
-                    />
-                    <p>thecat@hotmail.com</p>
-                </div>
-            </article>
-            
-            <article className="contact-card">
-                <img 
-                    src="src/assets/pumpkin.png"
-                    alt="Photo of Pumpkin"
-                />
-                <h3>Pumpkin</h3>
-                <div className="info-group">
-                    <img 
-                        src="src/assets/phone-icon.png" 
-                        alt="phone icon" 
-                    />
-                    <p>(0800) CAT KING</p>
-                </div>
-                <div className="info-group">
-                    <img 
-                        src="src/assets/mail-icon.png" 
-                        alt="mail icon"
-                    />
-                    <p>pumpkin@scrimba.com</p>
-                </div>
-            </article>
-            
-        </div>
+        <>
+            <Contact/>
+        </>
     )
 }
