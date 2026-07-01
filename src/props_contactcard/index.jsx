7ecf400a2409fc16efbd786769fa7270e7b97ec8
HarikaPenukonda <penukondaharika@gmail.com>
2026-07-01 15:09:42 -0500

    hardcoding the properties in the components

--- src/props_contactcard/index.jsx
+++ src/props_contactcard/index.jsx
@@ -12,10 +12,30 @@ export default function ContactCard() {
     return (
         <>
         <div className="contacts">
-            <Contact/>
-            <Contact/>
-            <Contact/>
-            <Contact/>
+            <Contact
+                img = "src/assets/mr-whiskerson.png"
+                name = "Mr. Whiskerson"
+                phone = "(212) 555-1234"
+                email = "mr.whiskaz@catnap.meow"
+            />
+            <Contact
+                img = "src/assets/fluffykins.png"
+                name = "Fluffykins"
+                phone = "(212) 555-2345"
+                email = "fluff@me.com"
+            />
+            <Contact
+                img="./images/felix.png"
+                name="Felix"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
+            <Contact 
+                img="./images/pumpkin.png"
+                name="Pumpkin"
+                phone="(0800) CAT KING"
+                email="pumpkin@scrimba.com"
+            />
         </div>
             
         </>
