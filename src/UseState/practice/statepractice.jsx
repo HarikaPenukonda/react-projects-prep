58b73c1246d4a4147ac978565e25976a5a0812ec
HarikaPenukonda <penukondaharika@gmail.com>
2026-07-16 22:58:38 -0500

    updating state with callback function

--- src/UseState/practice/statepractice.jsx
+++ src/UseState/practice/statepractice.jsx
@@ -1,20 +1,27 @@
 import React, { useState } from "react"
 import '../style.css'
 export default function AppOne() {
-    /**
-     * Challenge: 
-     * Create state to track our count value (initial value is 0)
-     * Don't forget to replace the hard-coded "0" with your new state
-     */
-
     const[count, setCount] = useState(0)
 
+    /**
+     * Note: if you ever need the old value of state
+     * to help you determine the new value of state,
+     * you should pass a callback function to your
+     * state setter function instead of using
+     * state directly. This callback function will
+     * receive the old value of state as its parameter,
+     * which you can then use to determine your new
+     * value of state.
+     */
+
     function add(){
-        setCount(count + 1)
+        setCount(prevCount => prevCount + 1)
     }
 
+    // Challenge: update `substract` to use a callback function
+
     function minus(){
-        setCount(count - 1)
+        setCount(prevCount => prevCount - 1)
     }
 
 
