14448480e9fa9c1cc05d3c04c79df2e1389946c5
HarikaPenukonda <penukondaharika@gmail.com>
2026-07-16 22:27:47 -0500

    destructring array of useState

--- src/UseState/index.jsx
+++ src/UseState/index.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import './index.css'
 
 export default function UseStatePractice(){
-    const result = useState("Yes") //  [Hello, ƒ()], returns an array
+    const [result,func] = useState("Yes") //  [Hello, ƒ()], returns an array
     console.log(result)
 
     return(
         <main>
             <h1 className="title">Is state important to know?</h1>
-            <button className="value">{result[0]}</button>
+            <button className="value">{result}</button>
         </main>
     )
 }
