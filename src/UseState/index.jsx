0269fee18b24c8f611e56d5e71f00735200bb548
HarikaPenukonda <penukondaharika@gmail.com>
2026-07-16 22:25:33 -0500

    intro to useState

--- src/UseState/index.jsx
+++ src/UseState/index.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import './index.css'
 
 export default function UseStatePractice(){
-    const result = useState("Hello") //  [Hello, ƒ()]
+    const result = useState("Yes") //  [Hello, ƒ()], returns an array
     console.log(result)
 
     return(
         <main>
             <h1 className="title">Is state important to know?</h1>
-            <button className="value">Yes</button>
+            <button className="value">{result[0]}</button>
         </main>
     )
 }
