ddd9a91183f1ad520c243effeb803137d16cd3e7
HarikaPenukonda <penukondaharika@gmail.com>
2026-07-31 11:02:36 -0500

    ternary - challenge 2

--- src/UseState/practice/ternarypractice.jsx
+++ src/UseState/practice/ternarypractice.jsx
@@ -1,24 +1,19 @@
 import '../tp.css'
 export default function TernaryApp() {
     /**
-     * Challenge: Replace the if/else below with a ternary
-     * to determine the text that should display on the page
+     * Challenge: Move your ternary directly inside the jsx
+     * so YES and NO are determined inside the H1
+     * 
+     * Hint: you will no longer need `answer` variable 
      */
-    const isGoingOut = false
+    const isGoingOut = true
     
-    let answer = isGoingOut ? "Yes" : "No"// 👈 Use ternary here
-    
-    // Remove the code below 👇 once your ternary is done
-    // if(isGoingOut === true) {
-    //     answer = "Yes"
-    // } else {
-    //     answer = "No"
-    // }
+    //let answer = isGoingOut ? "Yes" : "No"// 👈 Use ternary here
     
     return (
         <main>
             <h1 className="title">Do I feel like going out tonight?</h1>
-            <button className="value">{answer}</button>
+            <button className="value">{isGoingOut?"Yes":"No"}</button>
         </main>
     )
 }
