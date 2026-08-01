d3b993a492ccfd6846f5ceea1f9bae8697ba983e
HarikaPenukonda <penukondaharika@gmail.com>
2026-08-01 13:17:44 -0500

    code cleanup

--- src/ChefClaude/components/Main.jsx
+++ src/ChefClaude/components/Main.jsx
@@ -2,12 +2,6 @@ import { useState } from "react";
 
 export default function Main(){
 
-     /**
-     * Challenge: Update our app so that when the user enters a
-     * new ingredient and submits the form, it adds that new
-     * ingredient to our list!
-     */
-
     const [ingredients, setIngredients] = useState([])
 
     let ingredientListItems = ingredients.map(ingredient => (
@@ -15,21 +9,12 @@ export default function Main(){
     ))
 
     function handleSubmit(event){
-        /**
-         * Like before, don't worry about this FormData stuff yet.
-         * Just use the newIngredient below to help you finish the
-         * challenge.
-         */
 
         event.preventDefault();   // Prevents page refresh
-        // console.log("Form Submitted!!")
-        // grabbing the form data and displaying
         const formData = new FormData(event.currentTarget) 
         // event.currentTarget refers to the element on which the event listener is attached. the event listener is attached to the <form>.
         // FormData is a built-in JavaScript object. It scans the form and collects every input that has a name attribute.
         const newIngredient = formData.get("ingredient") // Give me the value stored under the key ingredient.
-        // ingredients.push(newIngredient)
-        // console.log(ingredients)
         setIngredients(prevIngredients => [...prevIngredients,newIngredient])
         console.log(ingredients)
     }
