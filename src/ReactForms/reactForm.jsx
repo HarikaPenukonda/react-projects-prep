810d71ddff355dda38337775ee4232d8832fbe97
HarikaPenukonda <penukondaharika@gmail.com>
2026-08-02 16:40:55 -0500

    form submission

--- src/ReactForms/reactForm.jsx
+++ src/ReactForms/reactForm.jsx
@@ -1,17 +1,31 @@
-import "./style.css"
+import "./form.css"
 export default function ReactFormApp(){
+
+    function handleSubmit(event){
+        event.preventDefault()
+        console.log("submitted")
+        const formEl = event.currentTarget
+        const formData = new FormData(formEl)
+        const email = formData.get("email")
+        console.log(email)
+        // gather the info from the form
+        // send it to backend
+    }
+
   return(
     <>
         <section>
             <h1>Signup Form</h1>
-            <form>
+            <form onSubmit={handleSubmit} method="post">
                 <label htmlFor="email">Email :</label>
                 <input id="email" type="email" name="email" placeholder="joe_schmoe@mail.com"/>
-                <br></br>
-                <br></br>
-                <label>Password :</label>
+                <br />
+               
+                <label htmlFor="password">Password :</label>
                 <input id="password" type="password" name="password" />
+                <br />
 
+                <button>submit</button>
             </form>
         </section>
     </>
