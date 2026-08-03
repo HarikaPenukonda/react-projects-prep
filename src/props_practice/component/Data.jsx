2115e0292e80e467458e0669ae4a94147f817aa8
HarikaPenukonda <penukondaharika@gmail.com>
2026-08-03 13:31:58 -0500

    show/hide punchline

--- src/props_practice/component/Data.jsx
+++ src/props_practice/component/Data.jsx
@@ -19,7 +19,8 @@ export default function Data(props){
             <div>
                 {isShown && <h4>Punchline : {props.punchline}</h4>}
             </div>
-            {isShown ? <button onClick={toggleButton}>hide punchline</button> : <button onClick={toggleButton}>show punchline</button>}
+            {/* {isShown ? <button onClick={toggleButton}>hide punchline</button> : <button onClick={toggleButton}>show punchline</button>} */}
+           <button onClick={toggleButton}>{isShown ? "hide" : "show"} punchline</button>
             <hr></hr>
         </>
     )
