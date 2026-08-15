3d3347db64a9bbb50ce55adb6e2849e54a62eedb
HarikaPenukonda <penukondaharika@gmail.com>
2026-08-15 15:14:26 -0500

    sound pads challenge 4.1 - local state

--- src/sound_pads/Pad.jsx
+++ src/sound_pads/Pad.jsx
@@ -1,4 +1,22 @@
+import { useState } from "react"
+
 export default function Pad(props){
+
+/**
+ * Challenge: Create state controlling whether
+ * this pad is "on" or "off". Use the incoming
+ * `props.on` to determine the initial state.
+ * 
+ * Create an event listener so when the pad is clicked,
+ * it toggles from "on" to "off".
+ * 
+ * Goal: clicking each pad should toggle it on and off.
+ */
+const [state,setState] = useState(props.on)
+
+function toggleClick(){
+    setState(prevState => !prevState)
+}
 
     const styles = {
         backgroundColor : props.color,
@@ -6,7 +24,7 @@ export default function Pad(props){
 
     return(
         <>
-            <button style={styles} className={props.on ? "on" : ""}></button>
+            <button onClick={toggleClick} style={styles} className={state ? "on" : ""}></button>
         </>
     )
 }
