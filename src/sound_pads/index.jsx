6f576d8bca02f57af0c30cdcceadb30e796e7b2e
HarikaPenukonda <penukondaharika@gmail.com>
2026-08-15 17:18:22 -0500

    notes

--- src/sound_pads/index.jsx
+++ src/sound_pads/index.jsx
@@ -8,7 +8,7 @@ export default function SPOne() {
     const [pads, setPads] = useState(padsData)
 
     function toggle(id){
-        setPads(prevPad => prevPad.map( // prevPad - array of pads [pad1,pad2,pad3,...], .map() gives you one individual item at a time.
+        setPads(prevPad => prevPad.map( // prevPad - array of pads [item1,item2,item3,...], .map() gives you one individual item at a time.
             item => {return item.id === id ? {...item, on : !item.on} : item }  // item → one individual pad   
         ))
     }
@@ -39,3 +39,11 @@ export default function SPOne() {
             </div>
         </main>
     )}
+
+/**
+ * before each component has individual state, and its own way of updating the state
+ * Now we have single source of truth that data flows in just one direction, it passes
+ * down through props to the components that needed and those props can trigger changes 
+ * through these call back functions or these functions being passed through props, which
+ * will trigger a change to the main single source of truth in the state of parent component
+ */
