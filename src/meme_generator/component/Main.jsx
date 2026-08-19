16f866c43e1defa5ce101c14622b69c32697a373
HarikaPenukonda <penukondaharika@gmail.com>
2026-08-18 21:37:46 -0500

    challenge : controlled components

--- src/meme_generator/component/Main.jsx
+++ src/meme_generator/component/Main.jsx
@@ -1,13 +1,6 @@
 import { useState } from "react"
 
 export default function Main() {
-     /**
-     * Challenge: move the hardcoded meme info into React
-     * state. Use an object with `topText`, `bottomText`,
-     * and `imageUrl` properties, and set the initial values to
-     * the ones hardcoded below.
-     */
-
     const[meme, setMeme] = useState({topText:"One does not simply",bottomText:"Walk into Mordor",imageUrl:"http://i.imgflip.com/1bij.jpg"})
     function handleChange(event){ 
         const {value,name} = event.currentTarget // object destructring
@@ -20,12 +13,6 @@ export default function Main() {
                 [name]: value becomes topText: "Hello"
             */ 
         }))
-        /**
-         * Challenge: update the topText value in the meme state
-         * object every time the topText input box is changed
-         * 
-         * Note: don't worry about bottomText at this point.
-         */
     }
     return (
         <main>
